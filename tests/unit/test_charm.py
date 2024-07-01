089628027e42e495fc20418504ce512c12a5996c
kayra1 <gemalmazkayra@gmail.com>
2024-07-01 15:06:43 +0300

    pytest unittest

--- tests/unit/test_charm.py
+++ tests/unit/test_charm.py
@@ -3,19 +3,17 @@
 #
 # Learn more about testing at: https://juju.is/docs/sdk/testing
 
-import unittest
-
 import ops
 import ops.testing
+import pytest
 from charm import GocertCharm
 
 
-class TestCharm(unittest.TestCase):
+class TestCharm:
+    @pytest.fixture(scope="function", autouse=True)
     def setUp(self):
         self.harness = ops.testing.Harness(GocertCharm)
-        self.addCleanup(self.harness.cleanup)
-        self.harness.begin()
 
-    def test_pebble_ready(self):
-        self.harness.container_pebble_ready("gocert")
-        self.assertEqual(self.harness.model.unit.status, ops.ActiveStatus())
+    def test_start_charm(self):
+        self.harness.begin_with_initial_hooks()
+        assert self.harness.model.unit.status == ops.ActiveStatus()
